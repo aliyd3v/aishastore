b9ca8b2a40c0c6bd9ef09e8ec72d30807be495c2
ali <nabiy5511@gmail.com>
2025-05-27 21:30:29 +0500

    Serverless database configs is wrote.

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,12 +7,15 @@ exports.PORT = process.env.PORT || 3000
 exports.ALGORITHM = process.env.ALGORITHM
 exports.CRYPTO_KEY = process.env.CRYPTO_KEY
 
-// PostgreSQL config.
+// PostgreSQL (local) config.
 exports.DB_PORT = process.env.DB_PORT
 exports.DB_HOST = process.env.DB_HOST
 exports.DB_USERNAME = process.env.DB_USERNAME
 exports.DB_PASS = process.env.DB_PASS
 exports.DB_DATABASE = process.env.DB_DATABASE
+
+// Supabase postgres (serverless) config.
+exports.DATABASE_URL = process.env.DATABASE_URL
 
 // TOKEN_TIME.
 exports.TOKEN_TIME = process.env.TOKEN_TIME
--- backend/src/database/postgres.js
+++ backend/src/database/postgres.js
@@ -1,13 +1,15 @@
 const postgres = require('pg')
-const { DB_PORT, DB_HOST, DB_USERNAME, DB_PASS, DB_DATABASE } = require('../config/config')
+const { DB_PORT, DB_HOST, DB_USERNAME, DB_PASS, DB_DATABASE, DATABASE_URL } = require('../config/config')
 
-const pg = new postgres.Client({
-    port: DB_PORT,
-    host: DB_HOST,
-    username: DB_USERNAME,
-    password: DB_PASS,
-    database: DB_DATABASE
-})
+// const pg = new postgres.Client({
+//     port: DB_PORT,
+//     host: DB_HOST,
+//     username: DB_USERNAME,
+//     password: DB_PASS,
+//     database: DB_DATABASE
+// })
+
+const pg = new postgres.Client({ connectionString: DATABASE_URL })
 
 pg.connect((err) => {
     if (err) console.error(err)
